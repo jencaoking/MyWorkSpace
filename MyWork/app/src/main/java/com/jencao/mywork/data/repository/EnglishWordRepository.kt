3478d37b718d816def63b6d4bd639754a2569fe5
jencaoking <663930657@qq.com>
2026-07-29 20:52:28 +0800

    feat(repository): 添加 touch 扩展函数导入

--- MyWork/app/src/main/java/com/jencao/mywork/data/repository/EnglishWordRepository.kt
+++ MyWork/app/src/main/java/com/jencao/mywork/data/repository/EnglishWordRepository.kt
@@ -2,6 +2,7 @@ package com.jencao.mywork.data.repository
 
 import com.jencao.mywork.data.local.dao.EnglishWordDao
 import com.jencao.mywork.data.local.entity.EnglishWordEntity
+import com.jencao.mywork.data.util.touch
 import javax.inject.Inject
 import javax.inject.Singleton
 
